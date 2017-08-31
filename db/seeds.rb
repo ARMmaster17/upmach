18a96d9a9873f2cffb0e516fed9a2fe453a16e2e
Joshua Zenn <jzenn@students.kennesaw.edu>
2017-08-31 21:28:57 +0000

    Dump upload from last work session.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,29 @@
-[Downtime, Part, Machine].each(&:delete_all)
+[Downtime, Part, Machine, User].each(&:delete_all)
 
-100.times do
-    machine = Machine.new
-    machine.name = Faker::App.name
-    machine.manufacturer = Faker::Company.name
-    machine.model = Faker::Hacker.abbreviation
-    machine.save
-    Faker::Number.between(10, 30).times do
-        part = Part.new
-        part.name = Faker::App.name
-        part.manufacturer = Faker::Company.name
-        part.model = Faker::Hacker.abbreviation
-        part.machine_id = machine.id
-        part.price = Faker::Commerce.price
-        part.save
-    end
-    Faker::Number.between(10, 30).times do
-        downtime = Downtime.new
-        downtime.start_time = Faker::Time.between(6.months.ago, Date.today, :all)
-        downtime.end_time = downtime.start_time + Faker::Number.between(5, 120).minutes
-        downtime.machine_id = machine.id
-        downtime.save
-    end
+machine = Machine.create do |m|
+    m.manufacturer = "Sample Manufacturer Inc"
+    m.model = "Example Machine 3000"
+    m.name = "1A"
+end
+
+part = machine.parts.create do |p|
+    p.manufacturer = "Sample Part Manufacturer LLC"
+    p.model = "Example Machine Part"
+    p.name = "L22-5A"
+end
+
+downtime = machine.downtimes.create do |d|
+    d.start_time = Time.now - 90
+    d.end_time = TIme.new - 30
+end
+
+admin = User.create do |a|
+    a.email = "admin@example.com"
+    a.encrypted_password = "uptime"
+    a.admin = true
+end
+
+user = User.create do |u|
+    u.email = "user@example.com"
+    u.encrypted_password = "worktime"
 end
